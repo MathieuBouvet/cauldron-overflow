1ebd38707c071120adf08d664f825da90e52682c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-10-20 16:50:24 +0200

    Update votes count on the clicked answer

--- public/js/questionShow.js
+++ public/js/questionShow.js
@@ -1,11 +1,12 @@
-var container = $('.js-vote-arrows');
-container.find('a').on('click', function (e) {
+var containers = $('.js-vote-arrows');
+containers.find('a').on('click', function (e) {
   e.preventDefault();
   var $link = $(e.currentTarget);
+  var answerId = $link.parent('.js-vote-arrows').data('answer-id');
   $.ajax({
     url: '/comments/10/vote/' + $link.data('direction'),
     method: 'POST',
   }).then(function (data) {
-    container.find('.js-vote-total').text(data.votes);
+    $('[data-answer-id="' + answerId + '"] .js-vote-total').text(data.votes);
   });
 });
