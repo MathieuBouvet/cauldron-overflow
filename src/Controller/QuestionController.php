cc11877dd097abc05711121e7084c5bce7f028e9
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-10-21 21:42:49 +0200

    Use some markdown formating

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
@@ -18,16 +20,30 @@ class QuestionController extends AbstractController
     /**
      * @Route("/question/{slug}", name="app_question_show")
      */
-    public function show($slug)
-    {
+    public function show(
+        $slug,
+        MarkdownParserInterface $parser,
+        CacheInterface $cache
+    ) {
+        $questionText =
+            "I've been turned into a cat, any *thoughts* on how to turn back?" .
+            " While I'm **adorable**, I don't really care for cat food.";
+        $cache->get('markdown_' . md5($questionText), function () use (
+            $parser,
+            $questionText
+        ) {
+            return $parser->transformMarkdown($questionText);
+        });
+        $parsedQuestionText = $parser->transformMarkdown($questionText);
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
+            'questionText' => $parsedQuestionText,
         ]);
     }
 }
